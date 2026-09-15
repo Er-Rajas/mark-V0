081f3ea1b1bd152c336decbb34d73bc2650c2f7e
Er-Rajas <rajasbhingarde18@gmail.com>
2026-09-15 23:39:09 +0530

    Revert "Release Mini Data Analyst Mk-V0-alpha"
    This reverts commit d2901d4c459ab168392a6ac9db1f3671c4a98ccb.

--- tools/loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
- 
-
-def Load_csv(file_path: str) -> pd.DataFrame:
-    """"
-    Load data from a csv file and returns a pandas DataFrame
-    """
-    return pd.read_csv(file_path)
